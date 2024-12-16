d4f535badf28ca3a8dd0b3754e17afe22ccc839c
sheng232 <lowellchessofficial@gmail.com>
2024-12-15 20:08:49 -0800

    added a footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,13 +1,16 @@
+import { Link } from "react-router-dom"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-import { faGithub } from "@fortawesome/free-brands-svg-icons"
+import { faGithub, faInstagram, faFacebook } from "@fortawesome/free-brands-svg-icons"
 export default function Footer(){
     return(
         <>
-            {/* <footer className="flex-center">
-                <p className="center">Lowell High School ©<br/>
-                    <a href="https://github.com/Sheng232/lsa-website" target="_blank" className="black-underline"><FontAwesomeIcon icon={faGithub} /> Check Out The Source Code</a>
-                </p> 
-            </footer> */}
+            <footer className="flex-center">
+                <div>
+                    <Link><FontAwesomeIcon icon={faInstagram} className="footer-icon"/></Link>
+                    <Link><FontAwesomeIcon icon={faFacebook} className="footer-icon"/></Link>
+                    <Link><FontAwesomeIcon icon={faGithub} className="footer-icon"/></Link>
+                </div>
+            </footer>
         </>
     )
 }
