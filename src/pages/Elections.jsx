12190d80e212e9bac8479bb60558fde61b82d42f
Sheng232 <Shengj12345@gmail.com>
2025-05-13 10:04:48 -0700

    feat: add election results display with dynamic rendering for multiple boards

--- src/pages/Elections.jsx
+++ src/pages/Elections.jsx
@@ -2,6 +2,72 @@ import { useState } from "react";
 import { Link } from "react-router-dom";
 
 export default function Elections({ electionData }) {
+    const ElectionResults = [
+        {
+            board: "SBC Election Results",
+            color: "red",
+            president: "Shoon Mon",
+            vicePresident: "Leona Cyster",
+            secretary: "Sydney Sharp",
+            treasurer: "Kaitlyn Huey",
+            publicRelations: "Karolyn Wong",
+            clubCoordinator: "Khuslen Tegshjargal",
+            danceCoordinator: "Peyton David",
+            communityLiaison: "Emily Gorodetskiy"
+            
+        },
+        {
+            board: "LSA 2026 Election Results",
+            color: "#9c1919",
+            president: "Bernice Chen",
+            vicePresident: "Max Marcelo-Tabios",
+            secretary: "Tracy Chau",
+            treasurer: "Bernie Zhen",
+            publicRelations: "Harrison Kashiwabara",
+            historian: "Jaden Yee"
+        },
+        {
+            board: "LSA 2027 Election Results",
+            color: "#5353f6",
+            president: "Riley Chon",
+            vicePresident: "Zeina Danfoura",
+            secretary: "Linda Nguyen",
+            treasurer: "Carson Lem",
+            publicRelations: "Emily Ma",
+            historian: "Leo Zhou"
+        },
+        {
+            board: "LSA 2028 Election Results",
+            color: "green",
+            president: "Dylan Mac",
+            vicePresident: "Andrew Bates",
+            secretary: "Fatima Shaikh",
+            treasurer: "Cameron Wee",
+            publicRelations: "NhaLan Vo",
+            historian: " Felix Meil"
+        },
+    ];
+    const displayElectionResults = ElectionResults.map((element, index)=>{
+        const {board, color, president, vicePresident, secretary, treasurer, publicRelations, historian, clubCoordinator, danceCoordinator, communityLiaison} = element
+        return(
+            <div key={index}>
+                <div className="election-outer-container">
+                    <div className="election-inner-container" style={{borderLeft: `6px solid ${color}`}}>
+                        <h3 style={{color: color}}>{board}</h3>
+                        <p><span className="bold">President: </span>{president}</p>
+                        <p><span className="bold">Vice President: </span>{vicePresident}</p>
+                        <p><span className="bold">Secretary: </span>{secretary}</p>
+                        <p><span className="bold">Treasurer: </span>{treasurer}</p>
+                        <p><span className="bold">Public Relations: </span>{publicRelations}</p>
+                        {historian && <p><span className="bold">Historian: </span>{historian}</p>}
+                        {clubCoordinator && <p><span className="bold">Club Coordinator: </span>{clubCoordinator}</p>}
+                        {danceCoordinator && <p><span className="bold">Dance Coordinator: </span>{danceCoordinator}</p>}
+                        {communityLiaison && <p><span className="bold">Community Liaison: </span>{communityLiaison}</p>}
+                    </div>
+                </div>
+            </div>
+        )
+    })
     // Extract Google Drive file ID
     function extractFileId(url) {
         const regex = /(?:\/file\/d\/|[?&]id=)([^/&?]+)/;
@@ -119,13 +185,16 @@ export default function Elections({ electionData }) {
             </div>
             
             <section className="info-page">
-                <div className="quick-links flex-center">
+                {/* <div className="quick-links flex-center">
                     <a href="#LSA 2028">LSA 2028 Elections</a>
                     <a href="#LSA 2027">LSA 2027 Elections</a>
                     <a href="#LSA 2026">LSA 2026 Elections</a>
                     <a href="#SBC">SBC Elections</a>
                 </div>
-                {renderedSections}
+                {renderedSections} */}
+                <div>
+                    {displayElectionResults}
+                </div>
             </section>
         </>
     );
