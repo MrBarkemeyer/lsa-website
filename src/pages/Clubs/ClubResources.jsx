fca6d638b9fd6424d17e95862244227aa9c51d59
wkarolyn <wkarolyn@gmail.com>
2025-10-20 09:26:05 -0700

    Update ClubResources.jsx

--- src/pages/Clubs/ClubResources.jsx
+++ src/pages/Clubs/ClubResources.jsx
@@ -11,11 +11,11 @@ export default function ClubResources(){
             <div className="club-resources">
                 <h1>Welcome Club Leaders and Students!</h1>
                 <p>Here you can find all the materials and resources to maintain, or start a new club in one place!</p>
-                <h2>Join the 2024-2025 Activities Google Classroom</h2>
-                <p><strong>Join Code:</strong> .....</p>
+                <h2>Join the 2025-2026 Activities Google Classroom</h2>
+                <p><strong>Join Code:</strong> knmn6yuw</p>
                 <p>Here you can find updates and announcements for clubs, such as sign-ups for co-curricular events, or important forms to fill out! Keep in mind that although the materials can be found here, you must join the google classroom to be able to submit docs, and receive important updates</p>
                 <h2><span></span>Main Contact Shirley Tran (SBC Club Coordinator)</h2>
-                <p>Email: lowell.clubcoord@gmail.com</p>
+                <p>Email: lowellclubcoord25@gmail.com</p>
                 <div className="club-buttons">
                     <LinkButton to="/Clubs/NewClub" noTarget={true}>How to Start a Club</LinkButton>
                     <LinkButton to="/Clubs/EventPlanning" noTarget={true}>Event Planning Resources</LinkButton>
