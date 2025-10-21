19d3b038d635834f8d09e84dd8a3d68928276efd
wkarolyn <wkarolyn@gmail.com>
2025-10-20 21:48:47 -0700

    Update Elections.jsx

--- src/pages/Elections.jsx
+++ src/pages/Elections.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
 
-export default function ElectionPage({ electionData }) {
+export default function ElectionPage({ electionData, displayElectionResults}) {
   // Optional helper (currently unused)
   function extractFileId(url) {
     const regex = /(?:\/file\/d\/|[?&]id=)([^/&?]+)/;
