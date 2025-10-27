09f28e2a324f337d3bc26da4c1bd5822805a2ad6
wkarolyn <wkarolyn@gmail.com>
2025-10-26 20:00:30 -0700

    Refactor DSA component for improved readability

--- src/pages/About/DSA.jsx
+++ src/pages/About/DSA.jsx
@@ -1,18 +1,18 @@
-import Barkemeyer from "../../assets/Barkemeyer2.jpg"
+import Barkemeyer from "../../assets/Barkemeyer2.jpg";
 
-export default function DSA(){
-    return(
-        <>
-            <div className="title">
-                <h1>Directors of Student Activities</h1>
-            </div>
-            <section className="dsa center">
-                <div>
-                    <img src="/assets/Barkemeyer2.jpg" alt="Mr. Barkemeyer" className="dsa-image" />
-                    <h2>JACOB BARKEMEYER</h2>
-                </div>
-                <p>APUSH & Leadership Teacher, Badminton Coach, Director of Student Activities</p>
-            </section>
-        </>
-    )
+export default function DSA() {
+  return (
+    <>
+      <div className="title">
+        <h1>Directors of Student Activities</h1>
+      </div>
+      <section className="dsa center">
+        <div>
+          <img src={Barkemeyer} alt="Mr. Barkemeyer" className="dsa-image" />
+          <h2>JACOB BARKEMEYER</h2>
+        </div>
+        <p>APUSH & Leadership Teacher, Badminton Coach, Director of Student Activities</p>
+      </section>
+    </>
+  );
 }
