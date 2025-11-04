54442abd7536e07901e66545ccf66abec49448c3
wkarolyn <wkarolyn@gmail.com>
2025-11-04 08:53:36 -0800

    Update Committee.jsx

--- src/pages/About/Committee.jsx
+++ src/pages/About/Committee.jsx
@@ -1,10 +1,57 @@
- return(
-        <>
-            <div className="title">
-                <h1>{params}</h1>
-            </div>
-            <section className="center">
-                <h2>Coming soon</h2>
-            </section>
-        </>
-    )
+import {Link, useParams} from "react-router-dom"
+import { useState, useEffect } from "react"
+
+export default function ClassBoard(props){
+    const params = useParams().BoardName;
+    const officers = props.officerData;
+        const [officerData, setOfficerData] = useState([]);
+        useEffect(()=>{
+            setOfficerData((officers.filter(officer => params === officer.Team)));
+        }, [props, params]);
+
+        function extractFileId(driveUrl) {
+            const match = driveUrl.match(/[?&]id=([^&]+)/);
+            return match ? match[1] : null; // Return the file ID if matched, otherwise null
+        }
+                
+
+        const displayOfficers = officerData.map((officer, index)=>{
+            return(
+                <div key={index}>
+                    <div className="team-member">
+                    <img src={`https://drive.google.com/thumbnail?id=${extractFileId(officer.Photo)}`} alt={officer.Name} className="team-member-photo" />
+                    <div>
+                        <h2><span className="team-member-role" style={{color: "#861212"}}>{officer.Role}</span> 
+                        <span className="team-member-name"> {officer.Name}</span></h2>
+                        <p className="team-member-description">{officer.Description}</p>
+                    </div>
+                </div>
+                </div>
+            )
+        })
+    
+        if (!officerData) {
+            return (
+                <LoadingTruck />
+            )
+        }
+            
+    
+        return(
+            <>
+                <h1 className="team-name center">
+                    Committees {params} 
+                    <br/> 2025-2026
+                </h1>
+                {/* <div className="team-info">
+                    <p><strong>LOCATION - </strong>The Cave (Room 80A) during 1st block Leadership</p>
+                    <p><strong>EMAIL - </strong>lowellhssbc@gmail.com</p>
+                    <p><strong>INSTAGRAM - </strong>@lowellhs</p>
+                    <p><strong>FACEBOOK PAGE - </strong>Lowell Student Association</p>
+                </div> */}
+                <div className="teams">
+                    {displayOfficers}
+                </div>
+            </>
+        )
+    }
