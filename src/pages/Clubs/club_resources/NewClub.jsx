7cc25f771bb413d222dff767a31fab84542d8cde
wkarolyn <wkarolyn@gmail.com>
2025-11-06 08:55:08 -0800

    Update NewClub.jsx
    2025-2026 version

--- src/pages/Clubs/club_resources/NewClub.jsx
+++ src/pages/Clubs/club_resources/NewClub.jsx
@@ -10,13 +10,13 @@ export default function NewClub(){
                 <LinkButton to="/Clubs/ClubResources" noTarget={true}>{"< back to club resources"}</LinkButton>
                 <p>Resources to get you started on how to plan an event for your club! Club events are DIFFERENT from fundraisers! If you are looking to plan a fundraiser, visit the fundraising resources page!</p>
                 <p>KEEP IN MIND that there are only resources that are listed here. If you are looking for more information, join the 2023-2024 Activities Google Classroom (g6sf3ss). </p>
-                <h2>Main Contact Shanice Lam (SBC Club Coordinator)</h2>
-                <p>Email: lowell.clubcoord@gmail.com</p>
+                <h2>Main Contact Enkhiinkhuslen Tegshjargal (SBC Club Coordinator)</h2>
+                <p>Email: lowellclubcoord25@gmail.com</p>
                 <h2>Step 1 Join the 2023-2024 Activities Google Classroom</h2>
-                <p>Join Code: g6sf3ss</p>
+                <p>Join Code: knmn6yuw</p>
                 <p>Here you can find updates and announcements for clubs, such as sign-ups for co-curricular events, or important forms to fill out! Keep in mind that although the materials can be found here, you must join the google classroom to be able to submit docs, and receive important updates</p>
                 <h2>Step 2 Complete the Necessary Forms</h2>
-                <p>Before you can start meetings, you must fill out a couple of forms and get them approved by the SBC Club Coordinator. All of these MUST be filled out by the deadline in order to begin the process of starting a new club. All forms should preferably be signed digitally. Please email lowell.clubcoord@gmail.com with any questions or concerns.</p>
+                <p>Before you can start meetings, you must fill out a couple of forms and get them approved by the SBC Club Coordinator. All of these MUST be filled out by the deadline in order to begin the process of starting a new club. All forms should preferably be signed digitally. Please email lowellclubcoord25@gmail.com with any questions or concerns.</p>
                 <div className="club-buttons">
                     <LinkButton>Petition for New Club</LinkButton>
                     <LinkButton>Club Registration Form</LinkButton>
